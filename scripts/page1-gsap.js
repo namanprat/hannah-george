f5e1ab78b31719154dacc8d4e986aacabf49f533
namanprat <59867991+namanprat@users.noreply.github.com>
2023-03-16 18:48:25 +0530

    v1.1

---
+++ scripts/page1-gsap.js
@@ -0,0 +1,20 @@
+    const sections = gsap.utils.toArray(".work-title");
+    const container = document.querySelector('#work');
+   
+    const tl = gsap.timeline({
+     
+       scrollTrigger: {
+            trigger: ".work-title",
+            start: "top 17%",
+            end: "top 1%",
+            ease: "expo.inOut",
+            delay: -1,
+            scrub: true,
+            //markers: true,
+            end: () => `+=${container.offsetWidth}`
+        }
+    })
+   
+     tl
+    .to(sections, { duration: 1, xPercent: -100 * (sections.length - 1), ease: "none" })
+    .to(container, { duration: 1, backgroundColor: '#0E0B0B', ease: "none" }, 0)
